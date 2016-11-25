208574653e4c326c5c4ec83db9ca59a089480061
GuoLiuFang <guoliufangopen@163.com>
2016-11-25 15:37:03 +0800

    添加了剩余的其他字段

--- LearnSome.py
+++ LearnSome.py
@@ -55,7 +55,7 @@ def getSpName(message):
             if ch_code == -1:
                 continue
             else:
-                result_list.append((sp_name,ch_code))
+                result_list.append((sp_tuple[0],sp_name,ch_code[0],ch_code[1],ch_code[2],ch_code[3]))
     if len(result_list) > 0:
         return result_list
     return -1
@@ -69,7 +69,7 @@ def getChargeConde(sp_name,message):
             if len(code_list) > 1:
                 for code in code_list:
                     if code in message:
-                        return code
+                        return (charge_tuple[0],charge_tuple[1],sp_charge_str,code)
     return -1
 # ---从这里开始是 main 函数入口
 dbConenectReference = MySQLdb.connect(host='192.168.12.66', user='tigerreport', passwd='titmds4sp',
@@ -83,28 +83,28 @@ charge_codes = executor.fetchall()
 # messageContent = fetchMessageByDay(sys.argv[1])
 # messageContent = fetchMessageByDay('2016-10-01')
 messageContent = fetchMessageAll()
-# csvfile = open("/data/sdg/guoliufang/other_work_space/ResultCsv.txt", mode='wa+')
-csvfile = open("/Users/LiuFangGuo/Downloads/ResultCsv.txt", mode='wa+')
+csvfile = open("/data/sdg/guoliufang/other_work_space/ResultCsv.txt", mode='wa+')
+# csvfile = open("/Users/LiuFangGuo/Downloads/ResultCsv.txt", mode='wa+')
 csvlist=[]
 for index in range(len(messageContent)):
     message = messageContent[index][2].encode(encoding='utf-8')
     # message = """订购提醒：您好！您已成功订购由北京中天华宇科技有限责任公司提供的税务杂志精编，10元/月（由中国移动代收费），72小时内退订免费。【发送6700至10086每月免费体验10GB咪咕视频定向流量，可以连续体验3个月！详情 http://url.cn/40C8anS 】"""
     isValid = getValidMessage(message)
     if not isValid:
-        csvlist.append((messageContent[index][0],messageContent[index][1],messageContent[index][2],-1,-1,-1))
+        csvlist.append((messageContent[index][0],messageContent[index][1],messageContent[index][2],-1,-1,-1,-1,-1,-1,-1))
         continue
     else:
         status = getStatus(message)
         if status > -1:
             sp_name = getSpName(message)
             if sp_name == -1:
-                csvlist.append((messageContent[index][0], messageContent[index][1], messageContent[index][2], -1,-1,-1))
+                csvlist.append((messageContent[index][0], messageContent[index][1], messageContent[index][2], -1,-1,-1,-1,-1,-1,-1))
                 continue
             else:
                 for i in sp_name:
-                    csvlist.append((messageContent[index][0], messageContent[index][1], messageContent[index][2], status, i[0],i[1]))
+                    csvlist.append((messageContent[index][0], messageContent[index][1], messageContent[index][2], status, i[0],i[1],i[2],i[3],i[4],i[5]))
         else:
-            csvlist.append((messageContent[index][0], messageContent[index][1], messageContent[index][2], -1,-1,-1))
+            csvlist.append((messageContent[index][0], messageContent[index][1], messageContent[index][2], -1,-1,-1,-1,-1,-1,-1))
             continue
 # write list
 # dbWriteResult = MySQLdb.connect(host='192.168.12.155', user='guoliufang', passwd='tiger2108', db='honeycomb',
