abdb3488f7a4d97dba402b51b6eb17dede91ff47
GuoLiuFang <guoliufangopen@163.com>
2017-08-10 17:15:49 +0800

    增加了广联达的邮件发送

---
+++ mail_by_glodon.py
@@ -0,0 +1,27 @@
+# coding:utf8
+from email.mime.text import MIMEText
+from sys import argv
+print "草拟吗"
+import smtplib
+
+
+# --------main--------------
+mail_host = "mail.glodon.com"
+mail_port = 587
+login_user = "guolf@glodon.com"
+mail_senter = "guolf@glodon.com"
+login_password = "goaway@0323CAONIMA"
+email_content = argv[3]
+message = MIMEText(_text=email_content, _charset="utf-8")
+message['Subject'] = argv[2]
+'''
+Try cathch 控制邮箱验证因为经常更换密码
+'''
+# try:
+    # smtObj = smtplib.SMTP_SSL(host=mail_host, port=mail_port)
+smtObj = smtplib.SMTP(host=mail_host, port=mail_port)
+#smtObj.starttls()
+smtObj.login(user=login_user, password=login_password)
+smtObj.sendmail(from_addr=mail_senter, to_addrs=argv[1], msg=message.as_string())
+# except:
+    # print "屌丝邮箱验证除了问题"
